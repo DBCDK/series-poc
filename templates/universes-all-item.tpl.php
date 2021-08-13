90c4234a69b3560ca0744265ccc92f0349796924
Arni Loftsson <arniloftsson10.com>
2021-08-13 12:53:50 +0000

    Universes

---
+++ templates/universes-all-item.tpl.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Modified version of the default theme implementation for displaying a single
+ * search result. The h3 title have been removed.
+ *
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_search_result()
+ * @see template_process()
+ */
+?>
+
+<?php if ($item) : ?>
+  <li class="ting-universe-all-item">
+
+      <div class="ting-universe-all-object-cover">
+         <?php print $item->cover ?>
+      </div>
+
+      <div class="ting-universe-all-object-content">
+
+        <div class="ting-universe-all-object-title">
+          <h2><?php print $item->title ?></h2>
+        </div>
+
+        <div class="ting-universe-all-object-abstract">
+           <?php print $item->abstract ?>
+        </div>
+        <div class="ting-universe-all-object-series">
+          <h2><?php print $item->series ?></h2>
+        </div>
+
+      </div>
+  </li>
+
+<?php endif; ?>
